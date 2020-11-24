381f0aa3aa030658367578e05b133b2c0310b7c9
ritasobrak <margarita.sobrak@khk.ee>
2020-11-24 11:48:42 +0000

    Add contact to HTML table by person object and user interface object

--- app.js
+++ app.js
@@ -22,22 +22,10 @@ function addContact(e) {
     if (firstName.value === '') {
         alert("Add new contact data!")
     } else {
-        console.log("Create contact");
         const person = new Person(firstName.value, lastName.value, city.value, street.value, postCode.value, phone.value);
-        // create tr
-        const tr = document.createElement('tr');
-
-        //create all td with person data
-        tr.innerHTML = `<td>${person.firstName}</td>
-                        <td>${person.lastName}</td>
-                        <td>${person.city}</td>
-                        <td>${person.street}</td>
-                        <td>${person.postcode}</td>
-                        <td>${person.phone}</td>
-        `;
-        // append tr to table
-        contacts.appendChild(tr);
-        console.log(contacts);
+        console.log("Create user interface");
+        const ui = new UI();
+        ui.addPersonToTable(person);
         e.preventDefault();
     }
 }
