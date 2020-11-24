2140c86be5afc75d6312616e601f2674f6f75516
ritasobrak <margarita.sobrak@khk.ee>
2020-11-24 10:59:57 +0000

    Add contact to Address book by person object

---
+++ Person.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(firstName, lastName, city, street, postcode, phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.street = street;
+        this.postcode = postcode;
+        this.phone= phone;
+    }
+}
